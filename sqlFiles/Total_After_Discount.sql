76035d849cf98ac335c3803804dc742e59192041
seancrossettie <seancrossettie@gmail.com>
2021-09-11 11:54:35 -0500

    fixed bug in shipping costs

--- sqlFiles/Total_After_Discount.sql
+++ sqlFiles/Total_After_Discount.sql
@@ -1,6 +1,14 @@
 -- What is the cost after discount for each order? Discounts should be applied as a percentage off.
 
-SELECT d.OrderId, SUM(d.Quantity * d.UnitPrice) [Total Price], d.Discount [Discount %], 
-	ROUND(SUM((d.Quantity * d.UnitPrice) * (1 - d.Discount)), 2) [Total After Discount]
-FROM [Order Details] d
-Group By d.OrderID, d.Discount
+
+SELECT 
+	o.OrderID [Order ID],
+	SUM(d.Quantity * d.UnitPrice) [Subtotal],
+	o.Freight [Shipping Cost],
+	SUM((d.Quantity * d.UnitPrice) + o.Freight) [Total After Shipping], 
+	d.Discount [Discount %], 
+	ROUND(SUM(((d.Quantity * d.UnitPrice) + o.Freight) * (1 - d.Discount)), 2) [Total After Discount]
+FROM Orders o
+JOIN [Order Details] d
+ON o.OrderID = d.OrderID
+GROUP BY o.OrderID, o.Freight, d.Discount
